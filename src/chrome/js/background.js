03e0f744818338473b145c9e2fd50917a4c47bb4
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-10-13 00:52:32 +0530

    Initial commit

---
+++ src/chrome/js/background.js
@@ -0,0 +1,63 @@
+function getVideoContainer() {
+  const videoContainer = Array.from(
+    document.getElementsByClassName("html5-video-container"),
+  );
+  return videoContainer.length > 0 ? videoContainer[0] : null;
+}
+
+function getVideoWrapper() {
+  return getVideoContainer() ? getVideoContainer().parentNode : null;
+}
+
+function getVideoPlayer() {
+  return getVideoContainer() ? getVideoContainer().firstChild : null;
+}
+
+function isAdShowing() {
+  const wrapper = getVideoWrapper();
+  return wrapper !== null
+    ? wrapper !== undefined && String(wrapper.className).includes("ad-showing")
+    : null;
+}
+
+function getSkipButton() {
+  const skipAdButton = Array.from(
+    document.getElementsByClassName("ytp-ad-skip-button ytp-button"),
+  );
+  return skipAdButton.length > 0 ? skipAdButton[0] : null;
+}
+
+function waitForPlayer() {
+  if (getVideoPlayer()) {
+    hookVideoPlayer();
+  } else {
+    setTimeout(() => {
+      waitForPlayer();
+    }, 200);
+  }
+}
+
+function hookVideoPlayer() {
+  const videoPlayer = getVideoPlayer();
+  videoPlayer.addEventListener("timeupdate", () => {
+    getSkipButton()?.click();
+  });
+
+  if (isAdShowing()) {
+    videoPlayer.currentTime = videoPlayer.duration - 1;
+    videoPlayer.pause();
+    videoPlayer.play();
+  }
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === "complete" &&
+    String(tab.url).includes("https://www.youtube.com/watch")
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: waitForPlayer,
+    });
+  }
+});
