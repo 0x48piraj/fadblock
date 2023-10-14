f98df8ade36951e08b326b930e60aec07e9ebbdf
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-10-14 22:50:28 +0530

    major: [port] fadblock over opera, opera-next

---
+++ src/opera/js/content.js
@@ -0,0 +1,56 @@
+function getVideoContainer() {
+  const videoContainer = Array.from(
+    document.getElementsByClassName("html5-video-container"),
+  );
+  return videoContainer.length > 0 ? videoContainer[0] : null;
+}
+
+function getVideoWrapper() {
+  return getVideoContainer() ? getVideoContainer().parentNode : null;
+}
+
+function getVideoPlayer() {
+  return getVideoContainer() ? getVideoContainer().firstChild : null;
+}
+
+function isAdShowing() {
+  const wrapper = getVideoWrapper();
+  return wrapper !== null
+    ? wrapper !== undefined && String(wrapper.className).includes("ad-showing")
+    : null;
+}
+
+function getSkipButton() {
+  const skipAdButton = Array.from(
+    document.getElementsByClassName("ytp-ad-skip-button ytp-button"),
+  );
+  return skipAdButton.length > 0 ? skipAdButton[0] : null;
+}
+
+function waitForPlayer() {
+  if (getVideoPlayer()) {
+    hookVideoPlayer();
+  } else {
+    setTimeout(() => {
+      waitForPlayer();
+    }, 200);
+  }
+}
+
+function hookVideoPlayer() {
+  const videoPlayer = getVideoPlayer();
+  videoPlayer.addEventListener("timeupdate", () => {
+    getSkipButton()?.click();
+  });
+
+  if (isAdShowing()) {
+    videoPlayer.currentTime = videoPlayer.duration - 1;
+    videoPlayer.pause();
+    videoPlayer.play();
+  }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message)
+    return true
+});
